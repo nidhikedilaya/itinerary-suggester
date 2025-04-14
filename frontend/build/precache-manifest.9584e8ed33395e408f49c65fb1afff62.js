401da4bb10db96676b381a0d68b434a5973997dd
Nidhi <nidhikedilaya@gmail.com>
2025-04-14 13:24:56 +0530

    again

---
+++ frontend/build/precache-manifest.9584e8ed33395e408f49c65fb1afff62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e9690b2472c12a7457a86e8c633959d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d18550deea1ca2460a2d",
+    "url": "/static/css/main.a39a4bae.chunk.css"
+  },
+  {
+    "revision": "823bf2ada3b7f6c2aa88",
+    "url": "/static/js/2.6f2b4fb8.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/static/js/2.6f2b4fb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d217092c058421f822",
+    "url": "/static/js/3.1c0bccc8.chunk.js"
+  },
+  {
+    "revision": "d18550deea1ca2460a2d",
+    "url": "/static/js/main.0676105a.chunk.js"
+  },
+  {
+    "revision": "52dd32f74c36fc003772",
+    "url": "/static/js/runtime-main.478fc3ec.js"
+  }
+]);
